41258a62db0a9bf8dc3d3612b56482c90a862398
Gabrijel Radovcic <>
2025-03-28 23:05:49 +0100

    Initial project structure

---
+++ src/dsl_research_assistant/citation_retrieval.py
@@ -0,0 +1,9 @@
+from llama_index import GPTSimpleVectorIndex
+
+def load_index(index_path="papers_index.json"):
+    index = GPTSimpleVectorIndex.load_from_disk(index_path)
+    return index
+
+def search_for_citations(query, index):
+    response = index.query(query)
+    return response
