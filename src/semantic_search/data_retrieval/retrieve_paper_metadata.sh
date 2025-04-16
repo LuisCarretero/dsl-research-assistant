f5166490c90e02c59b07641fb98c1a9a5951853d
LuisCarretero <luis.carretero@gmx.de>
2025-04-16 20:14:18 +0200

    Initial data pull pipeline for Euler

--- src/semantic_search/data_retrieval/retrieve_paper_metadata.sh
+++ src/semantic_search/data_retrieval/retrieve_paper_metadata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=semanticScholar-retrival
-#SBATCH --output=/Users/luis/Desktop/ETH/Courses/SS25-DSL/src/semantic_search/retrieve_paper_metadata.out
+#SBATCH --output=/cluster/home/lcarretero/workspace/dsl/dsl-research-assistant/src/semantic_search/data_retrieval/retrieve_paper_metadata.out
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=4G
@@ -10,13 +10,13 @@ module load eth_proxy
 
 source $HOME/python_envs/dsl-research-assistant/bin/activate
 
-cd $HOME/workspace/dsl/dsl-research-assistant/src/semantic_search
+cd $HOME/workspace/dsl/dsl-research-assistant/src
 
 echo ":- Starting preprocessing job..."
 echo ":- Python environment: $VIRTUAL_ENV"
 echo ":- Current directory: $(pwd)"
-echo ":- Running script: retrieve_paper_metadata.py"
+echo ":- Running script: -m semantic_search.data_retrieval.retrieve_paper_metadata"
 
-python retrieve_paper_metadata.py
+python -m semantic_search.data_retrieval.retrieve_paper_metadata
 
 echo ":- Preprocessing job completed."
